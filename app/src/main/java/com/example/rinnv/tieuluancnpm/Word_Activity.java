8492e3542ef3655d56cce87ea11317f84500d9be
rinnv <rinnv.spkt@gmail.com>
2017-04-13 22:01:49 +0700

    Speech to text v2

--- app/src/main/java/com/example/rinnv/tieuluancnpm/Word_Activity.java
+++ app/src/main/java/com/example/rinnv/tieuluancnpm/Word_Activity.java
@@ -67,6 +67,7 @@ public class Word_Activity extends AppCompatActivity implements SpellCheckerSess
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
                 RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, word);
         startActivityForResult(intent, SPEECH_RECOGNITION_CODE);
     }
 
